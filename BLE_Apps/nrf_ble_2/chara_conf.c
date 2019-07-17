8ddca7431733197f13617f1e1c4d324b23c511c4
Pascal Dornfeld <r2d2return@gmail.com>
2019-07-17 12:45:04 +0200

    fixed release build

--- BLE_Apps/nrf_ble_2/chara_conf.c
+++ BLE_Apps/nrf_ble_2/chara_conf.c
@@ -12,65 +12,53 @@
 uint32_t chara_conf_add(my_service_t *p_cus) {
   // properties that will be displayed to the central during service discovery
   ble_gatts_char_md_t char_md;
-  {
-    memset(&char_md, 0, sizeof(char_md));
-    char_md.char_props.read = 1;
-    char_md.char_props.write = 1;
-    char_md.char_props.notify = 1;
-    char_md.p_char_user_desc = NULL;
-    char_md.p_char_pf = NULL;
-    char_md.p_user_desc_md = NULL;
-    char_md.p_sccd_md = NULL;
+  memset(&char_md, 0, sizeof(char_md));
+  char_md.char_props.read = 1;
+  char_md.char_props.write = 1;
+  char_md.char_props.notify = 1;
+  char_md.p_char_user_desc = NULL;
+  char_md.p_char_pf = NULL;
+  char_md.p_user_desc_md = NULL;
+  char_md.p_sccd_md = NULL;
 
-    //  Read  operation on Cccd should be possible without authentication.
-    ble_gatts_attr_md_t cccd_md;
-    {
-      memset(&cccd_md, 0, sizeof(cccd_md));
-      BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
-      BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.write_perm);
-      cccd_md.vloc = BLE_GATTS_VLOC_STACK;
-      char_md.p_cccd_md = &cccd_md;
-    }
-  }
+  //  Read  operation on Cccd should be possible without authentication.
+  ble_gatts_attr_md_t cccd_md;
+  memset(&cccd_md, 0, sizeof(cccd_md));
+  BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.read_perm);
+  BLE_GAP_CONN_SEC_MODE_SET_OPEN(&cccd_md.write_perm);
+  cccd_md.vloc = BLE_GATTS_VLOC_STACK;
+  char_md.p_cccd_md = &cccd_md;
 
   // set the UUID, point to the attribute metadata and set the size of the characteristic
   ble_gatts_attr_t attr_char_value;
-  {
-    memset(&attr_char_value, 0, sizeof(attr_char_value));
-    attr_char_value.init_len = sizeof(chara_conf_packet_u);
-    attr_char_value.init_offs = 0;
-    attr_char_value.max_len = sizeof(chara_conf_packet_u);
+  memset(&attr_char_value, 0, sizeof(attr_char_value));
+  attr_char_value.init_len = sizeof(chara_conf_packet_u);
+  attr_char_value.init_offs = 0;
+  attr_char_value.max_len = sizeof(chara_conf_packet_u);
 
-    // set the properties
-    ble_gatts_attr_md_t attr_md;
-    {
-      memset(&attr_md, 0, sizeof(attr_md));
-      attr_md.vloc = BLE_GATTS_VLOC_STACK;
-      attr_md.rd_auth = 0;
-      attr_md.wr_auth = 0;
-      attr_md.vlen = 0;
+  // set the properties
+  ble_gatts_attr_md_t attr_md;
+  memset(&attr_md, 0, sizeof(attr_md));
+  attr_md.vloc = BLE_GATTS_VLOC_STACK;
+  attr_md.rd_auth = 0;
+  attr_md.wr_auth = 0;
+  attr_md.vlen = 0;
 
-      // open access (read / write)
-      ble_srv_cccd_security_mode_t secu;
-      {
-        memset(&secu, 0, sizeof(ble_srv_cccd_security_mode_t));
-        BLE_GAP_CONN_SEC_MODE_SET_OPEN(&secu.read_perm);
-        BLE_GAP_CONN_SEC_MODE_SET_OPEN(&secu.write_perm);
-        attr_md.read_perm = secu.read_perm;
-        attr_md.write_perm = secu.write_perm;
-      }
-      attr_char_value.p_attr_md = &attr_md;
-    }
+  // open access (read / write)
+  ble_srv_cccd_security_mode_t secu;
+  memset(&secu, 0, sizeof(ble_srv_cccd_security_mode_t));
+  BLE_GAP_CONN_SEC_MODE_SET_OPEN(&secu.read_perm);
+  BLE_GAP_CONN_SEC_MODE_SET_OPEN(&secu.write_perm);
+  attr_md.read_perm = secu.read_perm;
+  attr_md.write_perm = secu.write_perm;
+  attr_char_value.p_attr_md = &attr_md;
 
-    // uuid setting
-    ble_uuid_t ble_uuid;
-    {
-      memset(&ble_uuid, 0, sizeof(ble_uuid_t));
-      ble_uuid.type = p_cus->uuid_type;
-      ble_uuid.uuid = CHARA_CONF_UUID;
-      attr_char_value.p_uuid = &ble_uuid;
-    }
-  }
+  // uuid setting
+  ble_uuid_t ble_uuid;
+  memset(&ble_uuid, 0, sizeof(ble_uuid_t));
+  ble_uuid.type = p_cus->uuid_type;
+  ble_uuid.uuid = CHARA_CONF_UUID;
+  attr_char_value.p_uuid = &ble_uuid;
 
   // add our characteristic
   return sd_ble_gatts_characteristic_add(p_cus->service_handle, &char_md, &attr_char_value, &p_cus->chara_conf_handles);
