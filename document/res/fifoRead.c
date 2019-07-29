97546912869a41a8246b7eef7a2f51bc3af826a6
Pascal Dornfeld <r2d2return@gmail.com>
2019-07-29 15:31:03 +0200

    cleanup

--- document/res/fifoRead.c
+++
@@ -1,5 +0,0 @@
-int32_t lsm6dsl_fifo_raw_data_get(lsm6dsl_ctx_t *ctx, uint8_t *buffer, uint8_t len) {
-  int32_t ret;
-  ret = lsm6dsl_read_reg(ctx, LSM6DSL_FIFO_DATA_OUT_L, buffer, len);
-  return ret;
-}
